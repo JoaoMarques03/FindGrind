bc19f537e45bdbf1f64a1ec52376e272cb08e1de
JoaoMarques03 <jgmarques03@gmail.com>
2023-04-19 13:33:21 +0100

    Added users to db

--- populate.sql
+++ populate.sql
@@ -1,2 +1,4 @@
 insert into users (username, password, age, weight, height, gender, bmi, points, difficulty_selected)
 values ('Cacifo','adm',77,80,150,'F',80,0,1); 
+insert into users (username, password, age, weight, height, gender, bmi, points, difficulty_selected)
+values ('Goncalinho','adm',20,78,185,'M',22.79,0,1); 
