872bbf2777a1a752202525bae28368cb61dea443
JoaoMarques03 <jgmarques03@gmail.com>
2023-04-21 14:40:33 +0100

    Added Locations to the db

--- populate.sql
+++ populate.sql
@@ -2,3 +2,15 @@ insert into users (username, password, age, weight, height, gender, bmi, points,
 values ('Cacifo','adm',77,80,150,'F',80,0,1); 
 insert into users (username, password, age, weight, height, gender, bmi, points, difficulty_selected)
 values ('Goncalinho','adm',20,78,185,'M',22.79,0,1); 
+insert into workout_spot (location_name, longitude, latitude)
+values ('Parque da Bela Vista', 38.744401169766995, -9.12210042750371);
+insert into workout_spot (location_name, longitude, latitude)
+values ('Marvila', 38.746791163972496, -9.112037594600956);
+insert into workout_spot (location_name, longitude, latitude)
+values ('Graça Calisthenics Park', 38.72120570174017, -9.139257400380155);
+insert into workout_spot (location_name, longitude, latitude)
+values ('Parque Eduardo VII', 38.73012518221117, -9.158396082622005);
+insert into workout_spot (location_name, longitude, latitude)
+values ('Parque do Corredor Verde', 38.73558194107584, -9.160733114780443);
+insert into workout_spot (location_name, longitude, latitude)
+values ('Parque do Largo da Luz', 38.762767458976384, -9.183379415464149);
