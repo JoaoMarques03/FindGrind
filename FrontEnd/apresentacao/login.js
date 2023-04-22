ec2c2adb0b5a2503c3b367cfd6357850bb1a2210
JoaoMarques03 <jgmarques03@gmail.com>
2023-04-22 21:47:24 +0100

    Updated Login

--- FrontEnd/apresentacao/login.js
+++ FrontEnd/apresentacao/login.js
@@ -5,29 +5,29 @@ loginButton.addEventListener('click', () => {
   const password = document.querySelector('#password').value;
 
   console.log('Username:', username);
-console.log('Password:', password);
+  console.log('Password:', password);
 
-fetch('http://localhost:3000/login', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ username, password })
-})
-.then((response) => {
-  console.log('Response status:', response.status);
-  console.log('Response body:', response.body);
-  if (response.ok) {
-    console.log('Login successful');
-    window.location.pathname = "/index.html" // redirect user to dashboard
-  } else {
-    alert('Invalid username or password');
-  }
-})
-.catch((error) => {
-  console.error(error);
-  alert('An error occurred while logging in');
-});
+  fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username, password })
+  })
+  .then((response) => {
+    console.log('Response status:', response.status);
+    console.log('Response body:', response.body);
+    if (response.ok) {
+      console.log('Login successful');
+      window.location.href = "/index.html"; // redirect user to dashboard
+    } else {
+      alert('Invalid username or password');
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    alert('An error occurred while logging in');
+  });
 
   console.log('Request sent successfully');
 });
