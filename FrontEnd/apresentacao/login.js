544dabfeea24a8603b7d8f2cd83ee3f8d05557aa
JoaoMarques03 <jgmarques03@gmail.com>
2023-05-08 13:49:18 +0100

    Optimized Client-Side Code

--- FrontEnd/apresentacao/login.js
+++ FrontEnd/apresentacao/login.js
@@ -3,35 +3,36 @@ window.onload = function onLoad() {
   var form = document.getElementById("form-login");
   console.log(form);
 
-  form.onsubmit = function(event) {
+  form.onsubmit = async function(event) {
 
-      event.preventDefault();
+    event.preventDefault();
 
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-      fetch('http://localhost:3000/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
-    })
-    .then((response) => {
+    try {
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      });
+
       console.log('Response status:', response.status);
       console.log('Response body:', response.body);
+
       if (response.ok) {
         console.log('Login successful');
         window.location.href = "index.html"; // redirect user to dashboard
       } else {
         alert('Invalid username or password');
       }
-    })
-    .catch((error) => {
+    } catch (error) {
       console.error(error);
       alert('An error occurred while logging in');
-    });
+    }
 
     console.log('Request sent successfully');
-  }
-}
+  };
+};
