6ee4d5302187bd2a1f0d61a41486042c4fe7a45b
JoaoMarques03 <jgmarques03@gmail.com>
2023-05-15 15:43:14 +0100

    Added Polygon Code

--- FrontEnd/apresentacao/find.js
+++ FrontEnd/apresentacao/find.js
@@ -1,21 +1,21 @@
+let map;
+
 function initMap() {
   const mapCenter = { lat: 38.722440237883724, lng: -9.14647389958958 };
   const zoomLevel = 12;
 
-  const map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: zoomLevel,
     center: mapCenter,
   });
 
-  if (navigator.geolocation) {
+  if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const userLocation = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
-        console.log(userLocation);
-        console.log(GeolocationCoordinates.coords)
         map.setCenter(userLocation);
         const userMarker = new google.maps.Marker({
           position: userLocation,
@@ -23,14 +23,14 @@ function initMap() {
           title: "Your location",
         });
       },
-      () => {
-        console.log("Error: The Geolocation service failed.");
+      (error) => {
+        console.log("Error: The Geolocation service failed.", error);
       }
     );
   } else {
     console.log("Error: Your browser doesn't support geolocation.");
   }
-
+  
   const locations = [
     { lat: 38.744401169766995, lng: -9.12210042750371, title: "Parque da Bela Vista", info: "teste" },
     { lat: 38.746791163972496, lng: -9.112037594600956, title: "Marvila", info: "teste" },
@@ -96,4 +96,33 @@ function initMap() {
       map.setCenter({ lat: location.lat, lng: location.lng });
     }
   }
+
+  console.log(map);
+  createPolygon(); // Call the createPolygon function to display the polygon
 }
+
+function createPolygon() {
+  console.log('createPolygon called');
+  const polygonCoords = [
+    { lat: 38.744401169766995, lng: -9.12210042750371 },
+    { lat: 38.746791163972496, lng: -9.112037594600956 },
+    { lat: 38.72120570174017, lng: -9.139257400380155 },
+    { lat: 38.73012518221117, lng: -9.158396082622005 },
+    { lat: 38.73558194107584, lng: -9.160733114780443 },
+    { lat: 38.762767458976384, lng: -9.183379415464149 },
+    { lat: 38.744401169766995, lng: -9.12210042750371 } // Repeat the first point to close the loop
+  ];
+  
+  const polygon = new google.maps.Polygon({
+    paths: polygonCoords,
+    strokeColor: '#00FF00', 
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35,
+  });
+
+  polygon.setMap(map);
+}
+
+window.addEventListener('load', initMap);
