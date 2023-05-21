e864ecc8728c98a4222959ae1fa784385728f8ec
JoaoMarques03 <jgmarques03@gmail.com>
2023-05-21 17:38:49 +0100

    Updated server-side code

--- database.js
+++ database.js
@@ -1,25 +1,5 @@
 require('dotenv').config();
 const { Client } = require('pg');
-const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const cors = require('cors');
-const app = express();
-const PORT = 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(cors({
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-  origin: '*'
-}));
-
-app.use(session({
-  secret: 'mysecret',
-  resave: false,
-  saveUninitialized: true
-}));
 
 const client = new Client({
   host: process.env.DB_HOST,
@@ -37,42 +17,4 @@ client.connect((err) => {
   }
 });
 
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const result = await client.query('SELECT * FROM users WHERE username=$1 AND password=$2', [username, password]);
-    if (result.rows.length > 0) {
-      res.status(200).send('ok');
-    } else {
-      res.status(401).send('Invalid username or password');
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error fetching user');
-  }
-});
-
-
-app.get('/users', async (req, res) => {
-  try {
-    const result = await client.query('SELECT * FROM users');
-    res.send(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error fetching users');
-  }
-});
-
-app.get('/workout_spot', async (req, res) => {
-  try {
-    const result = await client.query('SELECT * FROM workout_spot');
-    res.send(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error fetching workout spots');
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+module.exports = client;
